f95e81b690b6a98e18e8b420e5641971b2b62a76
Daniel Porto <daniel.porto@gmail.com>
2020-01-21 18:28:54 +0000

    Adds a Main class to prepare for implementing the server

---
+++ src/cs/calc/server/Main.java
@@ -0,0 +1,12 @@
+package cs.calc.server;
+
+import javax.xml.ws.Endpoint;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Calculator web service is starting.");
+        // initalize the instance of the remote object
+        // publish the object
+    }
+}
