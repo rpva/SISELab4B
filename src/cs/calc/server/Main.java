e9a9eb5e163203084f59d66dc6324423a90f8871
Daniel Porto <daniel.porto@gmail.com>
2020-01-21 21:56:06 +0000

    Added Intellij project files to fix a bug with library import and reset the Main.

--- src/cs/calc/server/Main.java
+++ src/cs/calc/server/Main.java
@@ -1,13 +1,10 @@
 package cs.calc.server;
 
-import javax.xml.ws.Endpoint;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Calculator web service is starting.");
-        // initialize the object calculator
-        Endpoint.publish("http://localhost:8090/calcservice",calc);
-
+        // initalize the instance of the remote object
+        // publish the object
     }
 }
